683d4e922bbd6c6dbd675e2edf216c43864957ed
kalyanreddy1009 <masanipavan@gmail.com>
2024-12-13 18:59:15 +0530

    Update outputs.tf

--- terraform-infra/outputs.tf
+++ terraform-infra/outputs.tf
@@ -1,7 +1,7 @@
 output "jenkins_public_ip" {
-  value = google_compute_instance.jenkins.network_interface[0].access_config[0].nat_ip
+  value = length(google_compute_instance.jenkins.network_interface[0].access_config) > 0 ? google_compute_instance.jenkins.network_interface[0].access_config[0].nat_ip : "No public IP assigned"
 }
 
-output "gke_cluster_name" {
-  value = google_container_cluster.gke_cluster.name
+output "jenkins_internal_ip" {
+  value = google_compute_instance.jenkins.network_interface[0].network_ip
 }
