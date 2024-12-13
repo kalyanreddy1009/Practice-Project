d97cd5d830b6a84e5296ab0c21d297db6f8ea6a2
kalyanreddy1009 <masanipavan@gmail.com>
2024-12-13 18:13:43 +0530

    Update main.tf

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -2,7 +2,7 @@
 provider "google" {
   credentials = file("./maximal-cabinet-442109-b6-38a77e8b7647.json")
   project     = "maximal-cabinet-442109-b6"
-  region      = "us-south1"
+  region      = "us-east1"
 }
 
 # Alternative provider configuration with alias
