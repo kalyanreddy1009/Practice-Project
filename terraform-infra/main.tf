fd1b7f3af4c60d6f37b7cdc25e685d4893081be3
kalyanreddy1009 <masanipavan@gmail.com>
2024-12-13 22:42:03 +0530

    Update main.tf

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -1,4 +1,3 @@
-# Provider configuration
 provider "google" {
   credentials = file("./maximal-cabinet-442109-b6-38a77e8b7647.json")
   project     = "maximal-cabinet-442109-b6"
@@ -6,7 +5,7 @@ provider "google" {
 }
 
 resource "google_compute_network" "vpc_network" {
-  name = "webapp-vpc"
+  name                    = "webapp-vpc"
   auto_create_subnetworks = false
 }
 
@@ -24,8 +23,10 @@ resource "google_container_cluster" "gke_cluster" {
   private_cluster_config {
     enable_private_nodes   = true
     enable_private_endpoint = false
-    master_ipv4_cidr_block = "172.16.0.0/28"  # Reserved for GKE master
+    master_ipv4_cidr_block = "172.16.0.0/28"
   }
+
+  deletion_protection = false
 
   node_config {
     machine_type = "e2-medium"
@@ -53,7 +54,22 @@ resource "google_compute_instance" "jenkins" {
   network_interface {
     network    = google_compute_network.vpc_network.id
     subnetwork = google_compute_subnetwork.subnet.id
+
+    access_config {}
   }
 
   tags = ["http-server"]
+}
+
+resource "google_compute_firewall" "allow_http" {
+  name    = "allow-http"
+  network = google_compute_network.vpc_network.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
 }
