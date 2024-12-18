709cad254c539ad806bc0c0145eac98ca54a67b2
kalyanreddy1009 <masanipavan@gmail.com>
2024-12-18 13:27:07 +0530

    Update variables.tf

--- terraform-infra/variables.tf
+++ terraform-infra/variables.tf
@@ -1,12 +1,25 @@
-variable "project_id" {
-  default = "maximal-cabinet-442109-b6"
-}
+# variables.tf
 
 variable "region" {
-  default = "us-central1"
+  description = "The GCP region to deploy resources in."
+  type        = string
+  default     = "us-central1"  # Default region
 }
 
 variable "zone" {
-  default = "us-central1-a"
+  description = "The GCP zone to deploy resources in."
+  type        = string
+  default     = "us-central1-a"  # Default zone
 }
 
+variable "bucket_name" {
+  description = "The name of the GCS bucket to store state."
+  type        = string
+  default     = "maximal-cabinet-442109-b6-tf-state"  # Unique bucket name
+}
+
+variable "cluster_name" {
+  description = "The name of the GKE cluster."
+  type        = string
+  default     = "terraform-cluster"  # Default cluster name
+}
