78dacad78b89ec7e6fb6bf1c565b9d974dfd3919
kalyanreddy1009 <masanipavan@gmail.com>
2024-12-18 13:25:47 +0530

    Update main.tf

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -1,75 +1,46 @@
+# main.tf
+
 provider "google" {
-  credentials = file("./maximal-cabinet-442109-b6-38a77e8b7647.json")
-  project     = "maximal-cabinet-442109-b6"
-  region      = "us-central1"
+  project = "maximal-cabinet-442109-b6"  # Your GCP project ID
+  region  = var.region                    # Region from variables.tf
+  zone    = var.zone                      # Zone from variables.tf
 }
 
-resource "google_compute_network" "vpc_network" {
-  name                    = "webapp-vpc"
-  auto_create_subnetworks = false
+# Use existing Service Account (terraform-sa@maximal-cabinet-442109-b6.iam.gserviceaccount.com)
+resource "google_service_account" "sa" {
+  account_id   = "terraform-sa"  # The account ID (does not need to be the full email)
+  display_name = "Terraform Service Account"
+  email        = "terraform-sa@maximal-cabinet-442109-b6.iam.gserviceaccount.com"  # Provided email
 }
 
-resource "google_compute_subnetwork" "subnet" {
-  name          = "webapp-subnet"
-  ip_cidr_range = "10.0.0.0/16"
-  network       = google_compute_network.vpc_network.id
-  region        = var.region
+# Create IAM roles for the service account
+resource "google_project_iam_member" "sa_role" {
+  project = "maximal-cabinet-442109-b6"  # Your GCP project ID
+  role    = "roles/container.admin"      # You can change the IAM role as per your requirements
+  member  = "serviceAccount:${google_service_account.sa.email}"
 }
 
-resource "google_container_cluster" "gke_cluster" {
-  name     = "webapp-gke-cluster"
-  location = var.region
-
-  private_cluster_config {
-    enable_private_nodes   = true
-    enable_private_endpoint = false
-    master_ipv4_cidr_block = "172.16.0.0/28"
-  }
-
-  deletion_protection = false
+# Create a GKE Cluster
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name  # Cluster name from variables.tf
+  location = var.region        # Region from variables.tf
+  initial_node_count = 3
 
   node_config {
-    machine_type = "e2-medium"
-    disk_size_gb = 50
+    machine_type = "e2-medium"  # Change machine type as needed
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
   }
 
-  initial_node_count = 3
+  # Enable GKE features like the API
+  enable_network_policy = true
 }
 
-resource "google_compute_instance" "jenkins" {
-  name         = "jenkins-server"
-  machine_type = "e2-medium"
-  zone         = var.zone
-
-  boot_disk {
-    initialize_params {
-      image = "projects/debian-cloud/global/images/family/debian-11"
-      size  = 20
-    }
-  }
-
-  network_interface {
-    network    = google_compute_network.vpc_network.id
-    subnetwork = google_compute_subnetwork.subnet.id
-
-    access_config {}
-  }
-
-  tags = ["http-server"]
+# Create a GCS Bucket for Terraform state (optional but recommended)
+resource "google_storage_bucket" "app_storage" {
+  name          = var.bucket_name      # Bucket name from variables.tf
+  location      = "US"                 # Location set as "US" for simplicity
+  force_destroy = true                 # Allow deletion of non-empty buckets
 }
 
-resource "google_compute_firewall" "allow_http" {
-  name    = "allow-http"
-  network = google_compute_network.vpc_network.id
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["http-server"]
-}
