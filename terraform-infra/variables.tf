d4344ad975ff3fe31988f23b77f93723b3b24ed0
kalyanreddy1009 <masanipavan@gmail.com>
2024-12-18 18:01:05 +0530

    Update variables.tf

--- terraform-infra/variables.tf
+++ terraform-infra/variables.tf
@@ -1,12 +1,16 @@
+# variables.tf
+
 variable "region" {
   description = "The GCP region to deploy resources in."
   type        = string
+  default     = "us-central1"  # Default region
   default     = "us-west1"  # Default region
 }
 
 variable "zone" {
   description = "The GCP zone to deploy resources in."
   type        = string
+  default     = "us-central1-a"  # Default zone
   default     = "us-west1-a"  # Default zone
 }
 
@@ -20,22 +24,4 @@ variable "cluster_name" {
   description = "The name of the GKE cluster."
   type        = string
   default     = "terraform-cluster"  # Default cluster name
-}
-
-variable "jenkins_master_url" {
-  description = "The URL of the Jenkins master."
-  type        = string
-  default     = "http://your-jenkins-server:8080"  # Change to your Jenkins master URL
-}
-
-variable "jenkins_agent_name" {
-  description = "The name of the Jenkins agent."
-  type        = string
-  default     = "my-agent"  # Jenkins agent name
-}
-
-variable "jenkins_agent_secret" {
-  description = "The secret for the Jenkins agent."
-  type        = string
-  default     = "your-agent-secret"  # Replace with your Jenkins agent secret
 }
