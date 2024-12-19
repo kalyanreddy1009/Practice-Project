cc9e64fdcf6e5d62794d8a59b2c5ae42d7fc1ceb
kalyanreddy1009 <masanipavan@gmail.com>
2024-12-19 19:50:03 +0530

    Update outputs.tf

--- terraform-infra/outputs.tf
+++ terraform-infra/outputs.tf
@@ -9,7 +9,7 @@ output "cluster_endpoint" {
 }
 
 output "cluster_ca_certificate" {
-  value = google_container_cluster.primary.cluster_ca_certificate
+  value = google_container_cluster.primary.master_auth[0].cluster_ca_certificate
 }
 
 output "service_account_email" {
