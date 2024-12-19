1dc109149ef25e90457f009180a5d5086e1393cf
kalyanreddy1009 <masanipavan@gmail.com>
2024-12-19 18:37:42 +0530

    Update variables.tf

--- terraform-infra/variables.tf
+++ terraform-infra/variables.tf
@@ -27,3 +27,22 @@ variable "project_id" {
   description = "The GCP project ID"
   default     = "maximal-cabinet-442109-b6"
 }
+variable "trusted_ip_range" {
+  description = "Trusted IP range for accessing GKE master nodes"
+  default     = "203.0.113.0/24"  # Replace with your IP range
+}
+
+variable "subnet_cidr" {
+  description = "CIDR range for the subnet"
+  default     = "10.0.0.0/24"  # Adjust as necessary
+}
+
+variable "http_source_ranges" {
+  description = "Source IP ranges allowed to access HTTP services"
+  default     = ["0.0.0.0/0"]  # Open to all; restrict if necessary
+}
+
+variable "ssh_source_ranges" {
+  description = "Source IP ranges allowed to access SSH services"
+  default     = ["203.0.113.0/24"]  # Replace with your trusted IP range
+}
