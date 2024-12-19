29d73bbe1e11d947ff981c1bf6398bf162523a6e
kalyanreddy1009 <masanipavan@gmail.com>
2024-12-19 19:18:33 +0530

    Update outputs.tf

--- terraform-infra/outputs.tf
+++ terraform-infra/outputs.tf
@@ -9,3 +9,8 @@ output "cluster_endpoint" {
 output "cluster_kubeconfig" {
   value = google_container_cluster.primary.kube_config[0].raw_kube_config
 }
+# Output the service account key for future use or configuration (optional)
+output "service_account_key" {
+  value     = google_service_account_key.terraform_sa_key.private_key
+  sensitive = true
+}
