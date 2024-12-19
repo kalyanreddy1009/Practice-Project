94195f59674d11ba95a01c78e264f711d890639d
kalyanreddy1009 <masanipavan@gmail.com>
2024-12-19 19:08:25 +0530

    Update outputs.tf

--- terraform-infra/outputs.tf
+++ terraform-infra/outputs.tf
@@ -6,14 +6,6 @@ output "cluster_endpoint" {
   value = google_container_cluster.primary.endpoint
 }
 
-output "cluster_ca_certificate" {
-  value = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
-}
-
-output "bucket_name" {
-  value = google_storage_bucket.app_storage.name
-}
-
-output "service_account_email" {
-  value = data.google_service_account.sa.email
+output "cluster_kubeconfig" {
+  value = google_container_cluster.primary.kube_config[0].raw_kube_config
 }
