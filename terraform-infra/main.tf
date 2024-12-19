b244f1fb032df8620c8297d685e7dda6362e7c6e
kalyanreddy1009 <masanipavan@gmail.com>
2024-12-19 18:48:26 +0530

    Update main.tf

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -152,23 +152,3 @@ resource "google_compute_firewall" "allow_ssh" {
   source_ranges = var.ssh_source_ranges  # Restrict SSH access using variables.tf
   target_tags   = ["ssh-server"]
 }
-
-output "cluster_name" {
-  value = google_container_cluster.primary.name
-}
-
-output "cluster_endpoint" {
-  value = google_container_cluster.primary.endpoint
-}
-
-output "cluster_ca_certificate" {
-  value = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
-}
-
-output "bucket_name" {
-  value = google_storage_bucket.app_storage.name
-}
-
-output "service_account_email" {
-  value = data.google_service_account.sa.email
-}
