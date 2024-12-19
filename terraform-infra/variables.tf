7bc755f0286a431ba7beee220150cfd63c8402fc
kalyanreddy1009 <masanipavan@gmail.com>
2024-12-19 19:54:21 +0530

    Update variables.tf

--- terraform-infra/variables.tf
+++ terraform-infra/variables.tf
@@ -1,64 +1,50 @@
-# variables.tf
-
-# Google Cloud Configuration
-variable "gcp_credentials_file" {
-  description = "The path to the Google Cloud service account credentials JSON file"
-  type        = string
-  default     = "/home/masanipavan/Practice-Project/terraform-infra/maximal-cabinet-442109-b6-1352222c3e5e.json"
-}
-
 variable "gcp_project" {
   description = "The Google Cloud project ID"
   type        = string
-  default     = "maximal-cabinet-442109-b6"
 }
 
 variable "gcp_region" {
   description = "The Google Cloud region"
   type        = string
-  default     = "us-central1"
 }
 
-# GKE Cluster Configuration
+variable "gcp_credentials_file" {
+  description = "Path to the service account key file"
+  type        = string
+}
+
 variable "cluster_name" {
   description = "The name of the GKE cluster"
   type        = string
-  default     = "my-cluster"
 }
 
 variable "cluster_location" {
-  description = "The location for the GKE cluster"
+  description = "The location of the GKE cluster"
   type        = string
-  default     = "us-central1-a"
 }
 
 variable "node_count" {
-  description = "The initial node count for the GKE cluster"
+  description = "Number of nodes in the cluster"
   type        = number
-  default     = 1
 }
 
 variable "machine_type" {
-  description = "The machine type for the GKE nodes"
+  description = "Machine type for the nodes"
   type        = string
-  default     = "e2-medium"
 }
 
-# Google Cloud Storage Bucket for Jenkins
 variable "jenkins_bucket_name" {
-  description = "The name of the Jenkins storage bucket"
+  description = "The name of the Jenkins Google Storage Bucket"
   type        = string
-  default     = "maximal-cabinet-jenkins-bucket"
 }
 
 variable "jenkins_bucket_location" {
-  description = "The location for the Jenkins storage bucket"
+  description = "The location of the Jenkins Google Storage Bucket"
   type        = string
-  default     = "US"
 }
 
 variable "force_destroy" {
-  description = "Whether to force delete the bucket even if it contains objects"
+  description = "Whether to forcefully destroy the storage bucket"
   type        = bool
   default     = true
 }
