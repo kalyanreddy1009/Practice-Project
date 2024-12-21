2c824d8d4ca2481708c23f830dc354a35e4fb877
kalyanreddy1009 <masanipavan@gmail.com>
2024-12-21 17:04:09 +0530

    Update variables.tf

--- terraform-infra/variables.tf
+++ terraform-infra/variables.tf
@@ -1,50 +1,7 @@
-variable "gcp_project" {
-  description = "The Google Cloud project ID"
-  type        = string
+variable "project_id" {
+  default = "maximal-cabinet-442109-b6"
 }
 
-variable "gcp_region" {
-  description = "The Google Cloud region"
-  type        = string
-}
-
-variable "gcp_credentials_file" {
-  description = "Path to the service account key file"
-  type        = string
-}
-
-variable "cluster_name" {
-  description = "The name of the GKE cluster"
-  type        = string
-}
-
-variable "cluster_location" {
-  description = "The location of the GKE cluster"
-  type        = string
-}
-
-variable "node_count" {
-  description = "Number of nodes in the cluster"
-  type        = number
-}
-
-variable "machine_type" {
-  description = "Machine type for the nodes"
-  type        = string
-}
-
-variable "jenkins_bucket_name" {
-  description = "The name of the Jenkins Google Storage Bucket"
-  type        = string
-}
-
-variable "jenkins_bucket_location" {
-  description = "The location of the Jenkins Google Storage Bucket"
-  type        = string
-}
-
-variable "force_destroy" {
-  description = "Whether to forcefully destroy the storage bucket"
-  type        = bool
-  default     = true
+variable "region" {
+  default = "us-central1"
 }
