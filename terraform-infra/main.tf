be7b1a87f13110110e2c538ebfaf8e9797dfa8e7
kalyanreddy1009 <masanipavan@gmail.com>
2024-12-21 17:21:03 +0530

    Update main.tf

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -8,6 +8,7 @@ resource "google_container_cluster" "gke_cluster" {
   name               = "web-app-cluster"
   location           = "us-central1"
   initial_node_count = 1
+
   node_config {
     machine_type = "e2-medium"
   }
@@ -38,4 +39,43 @@ resource "google_storage_bucket" "jenkins_artifacts" {
 
 resource "random_id" "bucket_id" {
   byte_length = 8
+}
+
+# Create a firewall rule to allow HTTP access to Jenkins (Port 8080)
+resource "google_compute_firewall" "jenkins_http" {
+  name    = "allow-jenkins-http"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8080"]
+  }
+
+  source_ranges = ["0.0.0.0/0"] # Public access, change if you need more restricted access
+}
+
+# Optional: Add a firewall rule to allow SSH access to Jenkins and GKE instances from Cloud Shell IP (or your IP)
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "allow-ssh"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["35.185.177.132/32"] # Cloud Shell IP or your personal public IP (replace it if needed)
+}
+
+# Optional: You can add another rule for HTTPS access if Jenkins or other services are running on HTTPS (port 443)
+resource "google_compute_firewall" "allow_https" {
+  name    = "allow-https"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"] # Public access, change if more restricted access is needed
 }
