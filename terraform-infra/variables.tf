eab73b3f43380f673692fdbd47989d1e23b14ee6
kalyanreddy1009 <masanipavan@gmail.com>
2024-12-27 15:16:21 +0530

    Update variables.tf

--- terraform-infra/variables.tf
+++ terraform-infra/variables.tf
@@ -1,7 +1,35 @@
 variable "project_id" {
-  default = "maximal-cabinet-442109-b6"
+  description = "Google Cloud project ID"
+  type        = string
+  default     = "maximal-cabinet-442109-b6"
 }
 
 variable "region" {
-  default = "us-central1"
+  description = "Google Cloud region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "Google Cloud zone"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "trusted_ips" {
+  description = "List of trusted IPs for firewall rules"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
+variable "gke_node_machine_type" {
+  description = "Machine type for GKE nodes"
+  type        = string
+  default     = "e2-medium"
+}
+
+variable "jenkins_machine_type" {
+  description = "Machine type for Jenkins VM"
+  type        = string
+  default     = "e2-medium"
 }
