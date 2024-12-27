ef250311f01457b1da688acd7e5699fbe28804ce
kalyanreddy1009 <masanipavan@gmail.com>
2024-12-27 15:19:33 +0530

    Update main.tf

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -26,7 +26,7 @@ resource "google_container_node_pool" "primary_nodes" {
   node_config {
     machine_type = "e2-medium"
     disk_size_gb = 50  # Ensuring sufficient disk space
-    memory_gb    = 8   # Memory configuration for nodes
+    
   }
 }
 
